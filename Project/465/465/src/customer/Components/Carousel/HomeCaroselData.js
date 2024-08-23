29a875afef16defb1b7a08b313709e5b3afbabef
sujal402 <sujalkhakhi402@gmail.com>
2024-08-23 15:01:51 +0530

    Initial

---
+++ Project/465/465/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,23 @@
+export const homeCarouselData=[
+    {
+        // https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg
+        image:"https://m.media-amazon.com/images/G/31/img2020/fashion/WomensApparel2024/P0Brands/Ceelb/Bewakoof_770x500._CB558031264_.jpg",
+        path:"/women/clothing/lengha_choli"
+    },
+    {
+        // https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg
+        image:"https://m.media-amazon.com/images/G/31/img2020/fashion/WomensApparel2024/P0Brands/Ceelb/Levis_770x500._CB558031264_.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://m.media-amazon.com/images/G/31/img2020/fashion/WomensApparel2024/P0Brands/Ceelb/W_770x500._CB558031264_.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        // https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg
+        image:"https://m.media-amazon.com/images/G/31/img2020/fashion/WomensApparel2024/P0Brands/Ceelb/Nykd_770x500._CB558031264_.jpg",
+        path:"/women/clothing/women_saree"
+    }
+
+]
+
